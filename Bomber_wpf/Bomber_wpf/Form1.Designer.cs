8770b53aaedcd03de32fb8b9cad0416e74e98ce5
kepo4ka <konstantin.bugulov@mail.ru>
2017-12-14 20:07:06 +0300

    Генерация стен; Генерация бонусов
    1) Неразрушаемые стены в виде сетки, разрушаемые стены рандомно и в виде
    сетки по краям и в центре.
    Стены симметричны относительно Ox и Oy
    
    2) Бонусы генерируются только внутри разрушаемых стен

--- Bomber_wpf/Bomber_wpf/Form1.Designer.cs
+++ Bomber_wpf/Bomber_wpf/Form1.Designer.cs
@@ -34,7 +34,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel1.Location = new System.Drawing.Point(15, 15);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.MaximumSize = new System.Drawing.Size(450, 450);
             this.panel1.MinimumSize = new System.Drawing.Size(450, 450);
             this.panel1.Name = "panel1";
@@ -46,7 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(737, 484);
+            this.ClientSize = new System.Drawing.Size(737, 452);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
--- Bomber_wpf/Bomber_wpf/Form1.cs
+++ Bomber_wpf/Bomber_wpf/Form1.cs
@@ -19,33 +19,22 @@ namespace Bomber_wpf
             g = panel1.CreateGraphics();
             p = new Pen(Color.Black);
             sb = new SolidBrush(Color.DimGray);
+            gb = new GameBoard();
+            g = panel1.CreateGraphics();           
+
         }
 
         public int cw = 30;
         Graphics g;
         Pen p;
         SolidBrush sb;
-
+        GameBoard gb;
 
         private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            g = panel1.CreateGraphics();
-            p = new Pen(Color.Black);
-            for (int i = 1; i < 15; i++)
+        {           
+            for (int i = 0; i < gb.Cells.GetLength(0); i++)
             {
-                g.DrawLine(p, i * cw, 0, i * cw, 15 * cw);
-                g.DrawLine(p, 0, i * cw, 15 * cw, i * cw);
-
-               
-            }
-            
-
-
-            GameBoard gb = new GameBoard();
-
-            for (int i = 1; i < gb.Cells.GetLength(0)-1; i++)
-            {
-                for (int j = 1; j < gb.Cells.GetLength(1)-1; j++)
+                for (int j = 0; j < gb.Cells.GetLength(1); j++)
                 {
                     var tcell = gb.Cells[i, j];
                     if (tcell is Cell_indestructible)
@@ -55,14 +44,31 @@ namespace Bomber_wpf
                     else if (tcell is Cell_destructible)
                     {
                         PaintRect(tcell.X, tcell.Y, Color.Bisque);
+                      //  g.FillEllipse(new SolidBrush(Color.Red), tcell.X * cw, tcell.Y * cw, cw, cw);
+
                     }
-                    
                 }
             }
 
+            for (int i = 0; i < gb.Bonuses.Count; i++)
+            {
+                var tbonus = gb.Bonuses[i];
+                PaintElliple(tbonus.X, tbonus.Y, Color.Red);
+            }
+          
 
-            //Thread.Sleep(10);
+
+            DrawGrid();
+
+            //Thread.Sleep(103);
             //panel1.Refresh();
+        }
+
+
+        public void PaintElliple(int x, int y, Color cl)
+        {
+            sb = new SolidBrush(cl);
+            g.FillEllipse(new SolidBrush(cl), x * cw, y * cw, cw, cw);
         }
 
 
@@ -73,7 +79,26 @@ namespace Bomber_wpf
             g.FillRectangle(sb, x * cw, y * cw, cw, cw);
         }
 
-      
+
+        /// <summary>
+        /// Нарисовать сетку
+        /// </summary>
+        public void DrawGrid()
+        {
+            int w = gb.Cells.GetLength(0);
+            int h = gb.Cells.GetLength(0);
+           
+            for (int i = 0; i < w; i++)
+            {
+                g.DrawLine(p, i * cw, 0, i * cw, w * cw);                
+            }
+            for (int j = 0; j < h; j++)
+            {
+                g.DrawLine(p, 0, j * cw, h * cw, j * cw);
+            }
+        }
+
+
     }
 
 
@@ -160,24 +185,27 @@ namespace Bomber_wpf
         public Random rn = new Random();
 
 
-        public GameBoard(int _w, int _h, int _size, int _players_count)
+        public GameBoard(int _size,  int _players_count)
         {
-            W = _w;
-            H = _h;
+            W = _size;
+            H = _size;
         }
 
-        public GameBoard ()
+        public GameBoard()
         {
             int size = 15;
             W = size;
             H = size;
             GenerateBoard(size);
-            // Players = new List<Player>(2);
-            
+            GenerateBonuses(3);
+
         }
 
-
-        public void GenerateBoard(int size)
+        /// <summary>
+        /// Сгенерировать стенки на квадратном поле
+        /// </summary>
+        /// <param name="size"></param>
+        private void GenerateBoard(int size)
         {
             Cells = new Cell[size, size];
             int temp_rn = 0;
@@ -193,27 +221,146 @@ namespace Bomber_wpf
                 }
             }
 
-            for (int i = 1; i < Cells.GetLength(0) - 1; i++)
+            for (int i = 2; i < Cells.GetLength(0) - 2; i+=2)
             {
-                for (int j = 1; j < Cells.GetLength(1) - 1; j++)
+                int j = (Cells.GetLength(1) / 2);
+                Cells[i, j] = new Cell_destructible();
+
+                Cells[i, j].X = i;
+                Cells[i, j].Y = j;
+
+                j = 0;
+                Cells[i, j] = new Cell_destructible();
+
+                Cells[i, j].X = i;
+                Cells[i, j].Y = j;
+
+                j = Cells.GetLength(1) - 1;
+                Cells[i, j] = new Cell_destructible();
+
+                Cells[i, j].X = i;
+                Cells[i, j].Y = j;
+            }
+
+            for (int j = 2; j < Cells.GetLength(1) - 2; j+=2)
+            {
+                int i = (Cells.GetLength(0) / 2);
+                Cells[i, j] = new Cell_destructible();
+
+                Cells[i, j].X = i;
+                Cells[i, j].Y = j;
+
+                i = 0;
+                Cells[i, j] = new Cell_destructible();
+
+                Cells[i, j].X = i;
+                Cells[i, j].Y = j;
+
+                i = Cells.GetLength(0) - 1;
+                Cells[i, j] = new Cell_destructible();
+
+                Cells[i, j].X = i;
+                Cells[i, j].Y = j;
+            }
+
+
+            for (int i = 1; i < (Cells.GetLength(0) / 2); i++)
+            {
+                for (int j = 1; j < (Cells.GetLength(1) / 2); j++)
                 {
                     temp_rn = rn.Next(0, 10);
-                    if (temp_rn < 4)
+                    int ii = Cells.GetLength(0) - i - 1;
+                    int jj = Cells.GetLength(1) - j - 1;
+
+                    if (temp_rn < 5)
                     {
                         Cells[i, j] = new Cell_destructible();
                         Cells[i, j].X = j;
                         Cells[i, j].Y = i;
-                    }
-                    else if (temp_rn > 8)
+
+                        Cells[ii, jj] = new Cell_destructible();
+                        Cells[ii, jj].X = jj;
+                        Cells[ii, jj].Y = ii;
+
+                        Cells[ii, j] = new Cell_destructible();
+                        Cells[ii, j].X = j;
+                        Cells[ii, j].Y = ii;
+
+                        Cells[i, jj] = new Cell_destructible();
+                        Cells[i, jj].X = jj;
+                        Cells[i, jj].Y = i;
+                    }                    
+                }
+            }
+
+            for (int i = 1; i < Cells.GetLength(0)-1; i+=2)
+            {
+                for (int j = 1; j < Cells.GetLength(1)-1; j+=2)
+                {
+                    Cells[i, j] = new Cell_indestructible();
+                    Cells[i, j].X = j;
+                    Cells[i, j].Y = i;
+                }
+            }
+        }
+
+
+
+
+
+        private void GenerateBonuses(int bonuses_count)
+        {
+            bonuses = new List<Bonus>();
+
+            List<Cell> cells_dest = new List<Cell>();
+
+            for (int i = 0; i < W/2; i++)
+            {
+                for (int j = 0; j < H/2; j++)
+                {
+                    if (Cells[i,j] is Cell_destructible)
                     {
-                        Cells[i, j] = new Cell_indestructible();
-                        Cells[i, j].X = j;
-                        Cells[i, j].Y = i;
+                        cells_dest.Add(Cells[i, j]);
                     }
                 }
             }
-         
+
+            bonuses_count = bonuses_count % cells_dest.Count;
+
+
+            for (int i = 0; i < bonuses_count; i++)
+            {
+                int rpoint = rn.Next(0, cells_dest.Count);
+                int rtype = rn.Next(0, 2);
+                Bonus tbonus;
+                if (rtype%2==0)
+                {
+                    tbonus = new Bonus_multiple(cells_dest[rpoint].X, cells_dest[rpoint].Y);
+                    bonuses.Add(tbonus);
+
+                    bonuses.Add(tbonus);
+
+                    bonuses.Add(tbonus);
+
+                    bonuses.Add(tbonus);
+                }
+                else
+                {
+                    tbonus = new Bonus_big(cells_dest[rpoint].X, cells_dest[rpoint].Y);
+                    bonuses.Add(tbonus);
+
+                    bonuses.Add(tbonus);
+
+                    bonuses.Add(tbonus);
+
+                    bonuses.Add(tbonus);
+                }               
+            }
         }
+
+
+
+
 
 
         public int W
@@ -273,6 +420,17 @@ namespace Bomber_wpf
         {
             get
             {
+                //List<Bonus> visible_bombs = new List<Bonus>();
+
+                //for (int i = 0; i < bonuses.Count; i++)
+                //{
+                //    if (bonuses[i].Visible == true)
+                //    {
+                //        visible_bombs.Add(bonuses[i]);
+                //    }
+                //}
+                //return visible_bombs;
+
                 return bonuses;
             }
             set
@@ -312,7 +470,7 @@ namespace Bomber_wpf
     {
         int health;
         int id;
-
+    
         public int ID
         {
             get
@@ -355,6 +513,7 @@ namespace Bomber_wpf
     public class Bonus : GameObject
     {
         bool visible;
+
         public bool Visible
         {
             get
@@ -371,13 +530,25 @@ namespace Bomber_wpf
     [Serializable]
     public class Bonus_multiple : Bonus
     {
+       public Bonus_multiple(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            Visible = false;
+        }
+
 
     }
 
     [Serializable]
     public class Bonus_big : Bonus
     {
-
+        public Bonus_big(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            Visible = false;
+        }
     }
 
     [Serializable]
