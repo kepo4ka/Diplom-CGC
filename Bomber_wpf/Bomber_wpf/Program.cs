7e83aa97494c92c7c230f9e6dfbc704bac9f5c77
kepo4ka <konstantin.bugulov@mail.ru>
2018-02-01 22:45:33 +0300

    one exe file

--- Bomber_wpf/Bomber_wpf/Program.cs
+++ Bomber_wpf/Bomber_wpf/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using Bomber_wpf.Properties;
 
 namespace Bomber_wpf
 {
@@ -13,9 +17,53 @@ namespace Bomber_wpf
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += AppDomain_AssemblyResolve;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new StartPage());
+        }
+
+        /// <summary>
+        /// Событие возникающее при не возможности найти сборку
+        /// </summary>
+        private static Assembly AppDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine(args.Name);
+            Console.WriteLine("--------------------");
+
+            if (args.Name.Contains("User_class"))
+            {
+                // Console.WriteLine("Resolving assembly: {0}", args.Name);
+                MessageBox.Show("User_class");
+                // Загрузка запакованной сборки из ресурсов, ее распаковка и подстановка
+                using (var resource = new MemoryStream(Resources.User_class_dll))
+                using (var deflated = new DeflateStream(resource, CompressionMode.Decompress))
+                using (var reader = new BinaryReader(deflated))
+                {
+                    var one_megabyte = 1024 * 1024;
+                    var buffer = reader.ReadBytes(one_megabyte);
+                    return Assembly.Load(buffer);
+                }
+            }
+
+            else if (args.Name.Contains("ClassLibrary_CGC"))
+            {
+             //   Console.WriteLine("Resolving assembly: {0}", args.Name);
+
+                // Загрузка запакованной сборки из ресурсов, ее распаковка и подстановка
+                using (var resource = new MemoryStream(Resources.ClassLibrary_CGC_dll))
+                using (var deflated = new DeflateStream(resource, CompressionMode.Decompress))
+                using (var reader = new BinaryReader(deflated))
+                {
+                    var one_megabyte = 1024 * 1024;
+                    var buffer = reader.ReadBytes(one_megabyte);
+                    return Assembly.Load(buffer);
+                }
+            }
+
+            return null;
         }
     }
 }
--- OneExeProgram_source/scripts/Pack file with deflate algorithm.linq
+++ OneExeProgram_source/scripts/Pack file with deflate algorithm.linq
@@ -2,10 +2,10 @@
   <Namespace>System.IO.Compression</Namespace>
 </Query>
 
-var fileInputName = @"D:\Cloudmail\Исходники\C#\Diplom-CGC\OneExeProgram_source\scripts\ClassLibrary_CGC.dll";
+var fileInputName = @"D:\Cloudmail\Исходники\C#\Diplom-CGC\OneExeProgram_source\scripts\User_class.dll";
 var assembly = File.ReadAllBytes( fileInputName  );
 
-var fileOutputName = @"D:\Cloudmail\Исходники\C#\Diplom-CGC\OneExeProgram_source\scripts\ClassLibrary_CGC.dll.deflated";
+var fileOutputName = @"D:\Cloudmail\Исходники\C#\Diplom-CGC\OneExeProgram_source\scripts\User_class.dll.deflated";
 using( var file = File.Open( fileOutputName, FileMode.Create ) )
 using( var stream = new DeflateStream( file, CompressionMode.Compress ) )
 using( var writer = new BinaryWriter( stream ) )
