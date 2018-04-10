f54ae9a9638d5aa16820f9e3b6eab27c1f107b66
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-10 20:39:53 +0300

    Добавление класса UserInfo
    Класс содержит информацию о объектах, связатных с пользователями
    (Player, TcpClient, TimeLimit)

---
+++ Bomber_wpf_serverside/Bomber_wpf/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary_CGC;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Bomber_wpf
+{
+    public class UserInfo
+    {
+        public Player player;
+        public TcpClient client;
+        public int globalTimeLimit;
+
+        public UserInfo(Player pl, TcpClient cl)
+        {
+            player = pl;
+            client = cl;
+            globalTimeLimit = 0;
+        }
+    }
+}
