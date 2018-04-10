ff65291ff196ad2eac57c9b4e0317d6f5dc79638
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-10 22:19:55 +0300

    Класс, содержащий функции работы с Docker

---
+++ Bomber_wpf_serverside/Bomber_wpf/Docker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bomber_wpf
+{
+     class Docker
+    {
+        /// <summary>
+        /// Запустить docker контейнер
+        /// </summary>       
+        /// <returns>ID запущенного контейнера</returns>
+      public static string dockerRun()
+        {
+            string containerId = "";
+            bool isError = false;
+
+            for (int i = 0; i < runResult.Length; i++)
+            {
+                if (runResult[i] == ' ')
+                {
+                    isError = true;
+                    break;
+                }
+            }
+
+            if (!isError)
+            {
+                containerId = runResult;
+            }
+            return containerId;
+        }
+
+
+        public static void dockerStopContainer(string ID)
+        {
+        }
+
+
+    }
+}
