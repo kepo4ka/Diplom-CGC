76cd786170d0f2e897966d632a7cb211a843ff1f
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-10 19:54:56 +0300

    Изменение папок

--- Остальное/CGCClient From unother develop/CGCClient/PlayerStrategy/PlayerStrategy/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Player.h"
-
-int main(int argc, char** argv)
-{
-	Client::Strategy::Player player;
-
-	return 0;
-}
--- Остальное/CGCClient From unother develop/CGCClient/player/vs13/src/Common/Platform.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define INTERFACE_MEMBER(method) virtual method = 0
-#define IMPLEMENT(method) virtual method override
-#define BREAK __asm int 3
