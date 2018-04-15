462a1af2c263a191486b417f72323e1b78fc1702
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-15 19:50:00 +0300

    относительный путь вместо абсолютного
    Относительный путь до папки с примерами исходников стратегий

--- Bomber_console_server/Bomber_console_server/Program.cs
+++ Bomber_console_server/Bomber_console_server/Program.cs
@@ -21,13 +21,22 @@ namespace Bomber_console_server
         static string[] GetUserSourcePathsFromFile()
         {
             string[] usersPath = new string[4];
-            using (StreamReader sr = new StreamReader("users_source.txt"))
+            //using (StreamReader sr = new StreamReader("users_source.txt"))
+            //{
+            //    for (int i = 0; i < usersPath.Length; i++)
+            //    {
+            //        usersPath[i] = sr.ReadLine();
+            //    }
+            //}
+
+            int i = 0;
+            DirectoryInfo di = new DirectoryInfo(Path.GetFullPath($"{Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..")}\\User_sources"));
+            foreach (FileInfo fi in di.GetFiles())
             {
-                for (int i = 0; i < usersPath.Length; i++)
-                {
-                    usersPath[i] = sr.ReadLine();
-                }
+                usersPath[i] = fi.FullName;
+                i++;
             }
+
             return usersPath;
         }
     }
