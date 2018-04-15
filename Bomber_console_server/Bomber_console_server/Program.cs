e6faa0886b2e28c3129543f4a1890cd6d6e80761
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-15 19:14:22 +0300

    Перенесение сервера из Windows Form в консольное приложение

---
+++ Bomber_console_server/Bomber_console_server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bomber_console_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Session session = new Session(GetUserSourcePathsFromFile());
+            session.InitGame();
+            Console.WriteLine("ССЕсСИЯ игры завершена");
+            Console.ReadKey();
+        }
+
+        static string[] GetUserSourcePathsFromFile()
+        {
+            string[] usersPath = new string[4];
+            using (StreamReader sr = new StreamReader("users_source.txt"))
+            {
+                for (int i = 0; i < usersPath.Length; i++)
+                {
+                    usersPath[i] = sr.ReadLine();
+                }
+            }
+            return usersPath;
+        }
+    }
+}
