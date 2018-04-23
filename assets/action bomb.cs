36a468b3d6b9cadfd19bc847e20bea56712b2d8c
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-24 00:17:52 +0300

    доп файлы

---
+++ assets/action bomb.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary_CGC;
+
+namespace User_class
+{
+[Serializable]
+public class User : Player
+{
+public override PlayerAction Play(GameBoard gb)
+{
+Random rn = new Random();
+PlayerAction action;
+action = PlayerAction.Bomb;
+
+
+
+/*
+Код пользователя
+*/
+
+return action;
+}
+}
+}
