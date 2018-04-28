0f127df75cf8ee937d6fe760fdc343f77c5371ec
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-29 00:36:27 +0300

    добавление Клиента с возможностью отладки

--- cgc_client/User_class/User_class/User.cs
+++ cgc_client/User_class/User_class/User.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+
 using ClassLibrary_CGC;
 
 namespace User_class
@@ -10,15 +9,21 @@ namespace User_class
     [Serializable]
     public class User : Player
     {
+        /// <summary>
+        /// Задать Команду на следующий Тик
+        /// </summary>
+        /// <param name="gb">Игровое Поле</param>
+        /// <returns>Команда для Юнита</returns>
         public override PlayerAction Play(GameBoard gb)
         {
+            /*
+                Код пользователя
+            */
             Random rn = new Random();
             PlayerAction action;
-            action = PlayerAction.Wait;
-            
-            /*
-                 Код пользователя
-            */
+
+
+            action = (PlayerAction)rn.Next(0, 6);
 
             return action;
         }
